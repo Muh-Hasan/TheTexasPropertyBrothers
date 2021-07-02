b5d43cd8a458b9b2e6e70c5c097c71f5650d8ae8
Muh-Hasan <muh.hasan.git@gmail.com>
2021-07-02 20:12:06 +0500

    uncommented the code

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,28 +1,28 @@
-// const path = require("path")
-// exports.createPages = async ({ graphql, actions }) => {
-//   const { createPage } = actions
-//   const result = await graphql(`
-//     {
-//       allStrapiBlogs {
-//         nodes {
-//           title
-//           date
-//           blogImage {
-//             url
-//             name
-//           }
-//           description
-//         }
-//       }
-//     }
-//   `)
-//   result.data.allStrapiBlogs.nodes.forEach(item => {
-//     createPage({
-//       path: `/blogs/${item.title}`,
-//       component: path.resolve("./src/components/Template/BlogDisplay.js"),
-//       context: {
-//         data: item,
-//       },
-//     })
-//   })
-// }
+const path = require("path")
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    {
+      allStrapiBlogs {
+        nodes {
+          title
+          date
+          blogImage {
+            url
+            name
+          }
+          description
+        }
+      }
+    }
+  `)
+  result.data.allStrapiBlogs.nodes.forEach(item => {
+    createPage({
+      path: `/blogs/${item.title}`,
+      component: path.resolve("./src/components/Template/BlogDisplay.js"),
+      context: {
+        data: item,
+      },
+    })
+  })
+}
