5e9326b607573c9c8094c189679076f3cc7c7eab
Muh-Hasan <muh.hasan.git@gmail.com>
2021-07-02 20:09:11 +0500

    fix: footer problem

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,10 @@
 import React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
 import Logo from "../../assets/img/logo.png"
-import Facebook from '../../assets/img/facebook.png'
-import Twitter from '../../assets/img/twitter.png'
-import Instagram from '../../assets/img/instagram.png'
-import Linkedin from '../../assets/img/linkedin.png'
+import Facebook from "../../assets/img/facebook.png"
+import Twitter from "../../assets/img/twitter.png"
+import Instagram from "../../assets/img/instagram.png"
+import Linkedin from "../../assets/img/linkedin.png"
 
 const Footer = () => {
   const data = useStaticQuery(graphql`
@@ -15,9 +15,17 @@ const Footer = () => {
           uri
         }
       }
+      allStrapiSocialMediaLinks {
+        nodes {
+          twitter
+          linkedin
+          instagram
+          facebook
+        }
+      }
     }
   `)
-  const { allStrapiFooterLinks } = data
+  const { allStrapiFooterLinks, allStrapiSocialMediaLinks } = data
   return (
     <div className="footer">
       <div className="footer-1">
@@ -44,17 +52,17 @@ const Footer = () => {
         <div className="sec-3 footer-spacing">
           <div className="text-center">
             <div className="media">
-              <a>
+              <a href={allStrapiSocialMediaLinks.nodes[0].facebook}>
                 <img src={Facebook} />
               </a>
-              <a>
-                <i className="fa fa-twitter"></i>
+              <a href={allStrapiSocialMediaLinks.nodes[0].twitter}>
+                <img src={Twitter} />
               </a>
-              <a>
-                <i className="fa fa-linkedin"></i>
+              <a href={allStrapiSocialMediaLinks.nodes[0].instagram}>
+                <img src={Instagram} />
               </a>
-              <a>
-                <i className="fa fa-instagram"></i>
+              <a href={allStrapiSocialMediaLinks.nodes[0].linkedin}>
+                <img src={Linkedin} />
               </a>
             </div>
           </div>
